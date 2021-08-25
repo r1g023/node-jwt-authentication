e02e6f8e3dfa5608c38ecf8907a681781be54003
rigo0523 <limon1285@gmail.com>
2021-08-24 23:01:05 -0500

    server api up and running

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express();
+
+//GLOBACL MIDDLEWARE
+
+//IMPORT ROUTER
+
+//SERVER endpoints ---->
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require("./api/server");
+
+//PORT 5000
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}...`);
+});
