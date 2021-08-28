b4b30f2e58d6b42af96bde6863b3747c08aa26a7
rigo0523 <limon1285@gmail.com>
2021-08-28 12:08:48 -0500

    auth implemented again

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,30 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
+const { generateToken } = require("../middleware/global-middleware");
 
-router.post("/register", (req, res, next) => {});
+router.post("/register", (req, res, next) => {
+  const credentials = req.body;
+  const hashedPassword = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hashedPassword;
+  Users.registerUser(credentials)
+    .then((user) => {
+      const token = generateToken(user);
+      res.json({ user, token });
+    })
+    .catch((err) => next(err));
+});
 
-router.post("/login", (req, res, next) => {});
+router.post("/login", (req, res, next) => {
+  const credentials = req.body;
+  Users.loginUser({ username: credentials.username })
+    .then((user) => {
+      const token = generateToken(user);
+      user && bcrypt.compareSync(credentials.password, user.password)
+        ? res.json({ success: `welcome ${user.username} have a token`, token })
+        : res.json({ message: "invalid credentials" });
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
---
+++ middleware/global-middleware.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  generateToken,
+  restrictedUser,
+};
+
+function generateToken(user) {
+  const payload = {
+    subID: user.id,
+    name: user.username,
+    email: user.email,
+  };
+  const options = {
+    expiresIn: "1h",
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
+
+function restrictedUser() {
+  return async (req, res, next) => {
+    try {
+      const token = req.headers.authorization;
+      if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+          if (err) {
+            console.dir(err);
+            return res
+              .status(401)
+              .json({ message: "CANT LOGIN, INVALID TOKEN" });
+          } else {
+            req.decoded = decoded;
+            next();
+          }
+        });
+      } else {
+        res.json({ message: "you dont have access to this feature" });
+      }
+    } catch (error) {
+      console.log("Hello");
+      next(error);
+    }
+  };
+}
