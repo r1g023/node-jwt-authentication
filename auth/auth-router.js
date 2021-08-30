f438ec6e603bc98e4428c89c17d0f8b03bf8e003
rigo0523 <limon1285@gmail.com>
2021-08-30 16:39:32 -0500

    logout endpoint ended, removes cookie token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
-const { generateToken } = require("../middlware/global-middleware");
+const {
+  generateToken,
+  restrictedUser,
+} = require("../middlware/global-middleware");
 
 router.post("/register", (req, res, next) => {
   const credentials = req.body;
@@ -20,13 +23,23 @@ router.post("/login", (req, res, next) => {
   Users.loginUser({ username: credentials.username })
     .then((user) => {
       const token = generateToken(user);
+
       if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        res.cookie("token", token);
         res.json({ success: `welcome ${user.username} have a token!`, token });
       } else {
         res.json({ message: "please validate login" });
       }
     })
     .catch((err) => next(err));
+});
+
+router.get("/logout", async (req, res, next) => {
+  try {
+    res.clearCookie("token").end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- middlware/global-middleware.js
+++ middlware/global-middleware.js
@@ -14,12 +14,13 @@ function generateToken(user) {
   const options = {
     expiresIn: "1h",
   };
-  return jwt.sign(payload, process.env.JWT_SECRET, options);
+  const token = jwt.sign(payload, process.env.JWT_SECRET, options);
+  return token;
 }
 
 function restrictedUser() {
   return (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization || req.cookies.token;
     if (token) {
       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
