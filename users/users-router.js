7e88b04f875ff9873d78eb1f839d6f06dbcbb9bb
LimRi <57161327+rigo0523@users.noreply.github.com>
2021-09-14 22:59:43 -0500

    Update users-router.js

--- users/users-router.js
+++ users/users-router.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 
+//GET /api/users
 router.get("/", (req, res, next) => {
   Users.get()
     .then((user) => res.json(user))
     .catch((err) => next(err));
 });
 
+//GET /api/users/:id
 router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   Users.getById(id)
