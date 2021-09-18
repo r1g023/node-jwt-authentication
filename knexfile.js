e3a82148d02e7e04bf5181261065d5f64015560e
rigo0523 <limon1285@gmail.com>
2021-09-18 00:59:43 -0500

    admin on

--- knexfile.js
+++ knexfile.js
@@ -22,11 +22,19 @@ const { Client } = require("pg");
 // );
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || process.env.secretPG,
-  ssl: process.env.DATABASE_URL ? true : false,
+  connectionString: process.env.secretPG,
+  ssl: true,
 });
 
 client.connect();
+
+client.query("SELECT * FROM users;", (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const sharedConfig = {
   client: "pg",
