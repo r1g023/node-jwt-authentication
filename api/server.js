e80f2a26c0aaa2550ba69dfe0e24d368a57b3372
rigo0523 <limon1285@gmail.com>
2021-09-28 21:39:27 -0500

    update pg server on wsl linux

--- api/server.js
+++ api/server.js
@@ -13,12 +13,11 @@ const UserRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 const seiyaRouter = require("../saintSeiya/seiya-router");
 
-//SERVER endpoints --------->
+//SERVER endpoints ---->
 server.use("/", welcomeRouter);
-server.use("/api/users", restrictedUser(), UserRouter);
+server.use("/api/users", UserRouter);
 server.use("/api/auth", authRouter);
-server.use("/api/seiya", restrictedUser(), checkRole(), seiyaRouter);
-//
+server.use("/api/seiya", restrictedUser(), seiyaRouter);
 
 //middleware for CATCH ERROR on all endpoints of /api/messages
 server.use((err, req, res, next) => {
